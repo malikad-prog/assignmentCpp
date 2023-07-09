924c9325920b5c57b822dd97321d66dcd2cc4c4a
malikad-prog <mali.kadoshyan@gmail.com>
2023-07-09 22:25:09 +0300

    question2

--- q2/question02.cpp
+++ q2/question02.cpp
@@ -6,17 +6,26 @@
 // Make sure you compile the code in "Release" configuration (e.g O2 optimization level).
 // Do not modify the constexpr variables
 
+/* Explanation:
+    - Changed the gSum to be an atomic variable. 
+      It allow us to use a lock-free approach to change gSum without getting into a race condition.
+
+    - Created two threads - one for each operation. 
+*/
+
+
 #include <chrono>
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <atomic>
 
 constexpr size_t ITERATIONS = 1000*1000ULL;
 constexpr size_t OP1_PARAM = 2ULL;
 constexpr size_t OP2_PARAM = 1ULL;
 constexpr size_t EXPECTED_SUM = 1000001000000ULL;
 
-size_t gSum = 0;
+std::atomic<std::size_t> gSum = 0;
 
 void operation1(size_t arg) {
 	std::cout << "Performing operation1" << std::endl;
@@ -41,12 +50,15 @@ void operation2(size_t arg) {
 int main(int argc, char** argv)
 {
 	auto start = std::chrono::steady_clock::now();
-	operation1(OP1_PARAM);
-	operation2(OP2_PARAM);
+    std::thread t1(operation1, OP1_PARAM);
+    std::thread t2(operation2, OP2_PARAM);
+
+    t1.join();
+    t2.join();
 	auto end = std::chrono::steady_clock::now();
 
 	std::cout << "Total execution duration in milliseconds: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
-	std::cout << "Result:  " << sum <<  ", " <<(sum == EXPECTED_SUM ? "success" : "failure!")<< std::endl;
+	std::cout << "Result:  " << gSum/*sum*/ <<  ", " <<(gSum/*sum*/ == EXPECTED_SUM ? "success" : "failure!")<< std::endl;
 	std::cout << "Press enter to exit" << std::endl;
 	getchar();
 	return 0;
